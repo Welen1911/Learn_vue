d02f3bb0fde55db15dbfcea3f2fd2059c2afdacd
Welen1911 <welenalmeida2210@gmail.com>
2024-01-08 19:59:44 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
+  <TheHeader v-if="showHeader"/>
+  <div v-show="showName">
+    <ol>
+      <li>Nome: {{ name }}</li>
+      <li>Sobrenome: {{ lastName }}</li>
+    </ol>
+  </div>
+
+  <div v-if="accessLevel === 'admin'">Admin</div>
+  <div v-else-if="accessLevel === 'market'">Marketing</div>
+  <div v-else>Normal</div>
+
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, TheHeader
+  }, 
+  data() {
+    return {
+      showHeader: true,
+      name: 'Welen',
+      lastName: 'Almeida',
+      showName: true,
+      accessLevel: ''
+    }
   }
 }
 </script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="header">
+        <div v-if="count >= 2">
+            Maior ou igual a dois
+        </div>
+        <div v-else>
+            Menor que dois
+        </div>
+        <button v-on:click="count++">Acrescentar</button>
+    </header>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            count: 0
+            }
+        }
+    }
+</script>
+
+<style>
+    .header {
+        background: #000;
+        color: white;
+    }
+</style>
